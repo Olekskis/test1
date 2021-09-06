91026527403df1a9981e4453a40f65e33b77d1d4
Olekskis <oleksiwork@gmail.com>
2021-09-06 15:37:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+function login(password) {
+    // Password can be [a-zA-Z]
+    return password === "fSxA";
+}
+
+function brute(maxLength = 5) {
+    do {
+
+        // generate password
+
+        if (passwordLength > maxLength) {
+            return null;
+        }
+    } while (login(pasword));
+
+    return password;
+}
+
+let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+const password = "***Fs*";
+
+const password = "ac";
+const matrix = { 0: 0, 1: 2 }
+
+
+function getStringFromMatrix(allowedChars, matrix) {
+
+}
+
+getStringFromMatrix(arr, matrix)
+
+// console.log(arr.length)
--- readme.txt
+++
@@ -1,2 +0,0 @@
-Hello, Oleksii + Yan.
-Oleksii is here.
